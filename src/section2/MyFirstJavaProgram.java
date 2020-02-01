35f010f2b7fe596148688f40a1fc1faa1e0fd2f4
League Student <league@iMac-34.attlocal.net>
2020-02-01 15:56:46 -0800

    working on making shapes with a robot

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -6,9 +6,17 @@ import org.jointheleague.graphical.robot.Robot;
 public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
+		Robot jack = new Robot();
+		jack.setSpeed(10);
+		jack.penDown();
+		jack.move(100);
+		jack.turn(90);
+		jack.move(100);
+		jack.turn(90);
+		jack.move(100);
+		jack.turn(90);
+		jack.move(100);
 		
-		// START HERE
-	
 		
 		
 	}
