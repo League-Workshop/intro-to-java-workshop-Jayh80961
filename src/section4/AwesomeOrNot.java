12f8bcc92d31fc4cde85a001d15b9d5e68c7f7bf
League Student <league@iMac-17.attlocal.net>
2020-02-15 15:50:43 -0800

    I did very very very very well
    I did very very very very well

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,32 +2,63 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
-
+	public static void main(String[] args) {
+		
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+	Random random = new Random();
+	int randomNumber = random.nextInt(10);
 	// 3. Print out this variable
-
+	
 	// 4. Get the user to enter something that they think is awesome
-
+	String bob = JOptionPane.showInputDialog("Enter something awsome!!!");
 	// 5. If the random number is 0
-
+	JOptionPane.showMessageDialog(null, randomNumber);
+	
+	if(randomNumber == 0) { 
+		JOptionPane.showMessageDialog(null, "The thing that you entered is extremely very very bad.");
+	
+	}
+	else if (randomNumber == 1) { 
+		JOptionPane.showMessageDialog(null, "The thing that you entered is extremely very bad.");
+	}
 	// -- tell the user whatever they entered is awesome!
-
+	else if (randomNumber == 2) { 
+		JOptionPane.showMessageDialog(null, "The thing that you entered is extremely bad.");
+	}
 	// 6. If the random number is 1
-
+	else if (randomNumber == 3) { 
+		JOptionPane.showMessageDialog(null, "The thing that you entered is bad.");
+	}
 	// -- tell the user whatever they entered is ok.
-
+	else if (randomNumber == 4) { 
+		JOptionPane.showMessageDialog(null, "The thing that you entered is ok.");
+	}
 	// 7. If the random number is 2
-
+	else if (randomNumber == 5) { 
+		JOptionPane.showMessageDialog(null, "The thing that you entered is good.");
+	}
 	// -- tell the user whatever they entered is boring.
-
+	else if (randomNumber == 6) { 
+		JOptionPane.showMessageDialog(null, "The thing that you entered is extremely good");
+	}
 	// 8. If the random number is 3
-
+	else if (randomNumber == 7) { 
+		JOptionPane.showMessageDialog(null, "The thing that you entered is extremely very good");
+	}
 	// -- write your own answer
-
+	else if (randomNumber == 8) { 
+		JOptionPane.showMessageDialog(null, "The thing that you entered is extremely very very good");
+	}
+	else if (randomNumber == 9) { 
+		JOptionPane.showMessageDialog(null, "The thing that you entered is extremely very very very very very very very op");
+	}
+	
+	}
 }
 
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,42 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score = 0;
 		// 2.  Ask the user a question 
-		
+		String input = JOptionPane.showInputDialog("How is the weather today? Please answer in bad or good");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if(input.equalsIgnoreCase("bad")) {
+			// JOp.SMD
 		// 4.  if the user's answer was correct, add one to their score 
-		
+		}else if(input.equalsIgnoreCase("good")) {
+			score ++;
+		}
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
+		String input2 = JOptionPane.showInputDialog("Is computer better or a ipad better?");
 		
+		if(input2.equalsIgnoreCase("computer")) { 
+			score ++;
+			// JOp.SMD
+		}else if(input2.equalsIgnoreCase("ipad")) { 
+			
+		}
+		String input3 = JOptionPane.showInputDialog("Which one do you prefer, minecraft or roblox");
+
+		if(input3.equalsIgnoreCase("minecraft")) {
+			// JOp.SMD
+			score ++;
+		
+		}else if(input3.equalsIgnoreCase("roblox")) {
+			score ++;
+		}
 		// 6.  After all the questions have been asked, print the user's score 
-		
+		JOptionPane.showMessageDialog(null,"Wow your score is " + score + "!!! Impressive.....");
 	}
 }
+
